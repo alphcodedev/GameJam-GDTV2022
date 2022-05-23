d671c9de1e4bc2a5aef335c6ac3e504f2188acf1
Ariel Fernandez <bzk.hwl@gmail.com>
2022-05-23 03:37:02 -0400

    Added player basic animations. Wall and ceiling crawling implemented.

--- GameJam GameDevTV/Assets/Scripts/PlayerController.cs
+++ GameJam GameDevTV/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
     private float _moveInput;
+    private float _verticalInput;
 
-    private bool _facingRight = true;
+    public bool _facingRight = true;
+
+    public bool FacingRight
+    {
+        get => _facingRight;
+        set => _facingRight = value;
+    }
 
     private Rigidbody2D _rb;
     private Animator _anim;
 
+    public bool _verticalMovement = false;
+    private bool _isGhost = false;
+
+    public bool VerticalMovement
+    {
+        get => _verticalMovement;
+        set => _verticalMovement = value;
+    }
+
     // Jump Variables
-    private bool _isGrounded;
+    public bool _isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
@@ -33,6 +50,12 @@ public class PlayerController : MonoBehaviour
     public bool _wallSliding;
     public float wallSlidingSpeed;
 
+    // private bool _wallGrab = false;
+    private bool _possess = false;
+
+    public Transform topCheck;
+
+
     // Wall Jump Variables
     private bool wallJumping;
     public float xWallForce;
@@ -42,9 +65,17 @@ public class PlayerController : MonoBehaviour
     // Animator Hashes
     private static readonly int s_WalkingHash = Animator.StringToHash("walking");
     private static readonly int s_JumpingHash = Animator.StringToHash("jumping");
+    private static readonly int s_GhostHash = Animator.StringToHash("ghost");
+    private static readonly int s_PossessHash = Animator.StringToHash("possess");
 
-    // Const
+    // Constants
     private const int k_RemainingJumpsAmount = 1;
+    private const int k_GravityScale = 3;
+    private const float k_PossessColliderXOffset = 0.88f;
+    private const float k_PossessFrontCheckerXOffset = k_PossessColliderXOffset / 2;
+
+    // This is set to true when player is on left or top wall
+    public bool inverseMovement = false;
 
     void ResetExtraJumps()
     {
@@ -54,28 +85,34 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _anim = GetComponent<Animator>();
+        _anim = GetComponentInChildren<Animator>();
         ResetExtraJumps();
     }
 
     void Update()
     {
-        _moveInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _isGhost = !_isGhost;
+        }
 
-        HandleFlip();
+        _moveInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
 
         if (_isGrounded)
         {
             ResetExtraJumps();
         }
 
-        HandleMovement();
+        PerformMovement();
         HandleJump();
         HandleWallSlide();
-        HandleWallJump();
+        // HandleWallJump();
 
         _anim.SetBool(s_WalkingHash, _moveInput != 0);
         _anim.SetBool(s_JumpingHash, !_isGrounded);
+        _anim.SetBool(s_GhostHash, _isGhost);
+        _anim.SetBool(s_PossessHash, _possess);
     }
 
     private void HandleJump()
@@ -90,24 +127,25 @@ public class PlayerController : MonoBehaviour
             jumpHangCounter -= Time.deltaTime;
         }
 
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // Player can't Jump when it's Possessing
+        if (Input.GetKeyDown(KeyCode.Space) && !_possess)
         {
             // Handle Jump Buffer Time
             jumpBufferCount = jumpBufferLength;
 
-            // Handle Wall Slide
-            if (_wallSliding)
-            {
-                wallJumping = true;
-                Invoke(nameof(ResetWallJumping), wallJumpTime);
-            }
+            // // Handle Wall Grab
+            // if (_wallSliding)
+            // {
+            //     wallJumping = true;
+            //     Invoke(nameof(ResetWallJumping), wallJumpTime);
+            // }
         }
 
         // Perform the Jump
         if (jumpBufferCount > 0 && _remainingJumps > 0 && jumpHangCounter > 0)
         {
-            _rb.velocity = Vector2.up * jumpForce;
+            // _rb.velocity = Vector2.up * jumpForce;
+            _rb.AddRelativeForce(Vector2.up * jumpForce);
             jumpBufferCount = 0;
             _remainingJumps--;
         }
@@ -117,22 +155,28 @@ public class PlayerController : MonoBehaviour
             jumpBufferCount -= Time.deltaTime;
 
         // If we're moving upwards when we release the jump button
-        if (Input.GetKeyUp(KeyCode.UpArrow) && _rb.velocity.y > 0)
+        if (Input.GetKeyUp(KeyCode.Space) && _rb.velocity.y > 0)
         {
             _rb.velocity = new(_rb.velocity.x, _rb.velocity.y * .5f);
             jumpHangCounter = 0;
         }
     }
 
-    private void HandleFlip()
+    public void HandleFlip()
     {
-        if (!_facingRight && _moveInput > 0)
+        if (!_verticalMovement)
         {
-            Flip();
+            if (!_facingRight && _moveInput > 0 || _facingRight && _moveInput < 0)
+            {
+                Flip();
+            }
         }
-        else if (_facingRight && _moveInput < 0)
+        else
         {
-            Flip();
+            if (!_facingRight && _verticalInput > 0 || _facingRight && _verticalInput < 0)
+            {
+                Flip();
+            }
         }
     }
 
@@ -142,22 +186,109 @@ public class PlayerController : MonoBehaviour
         _isOnWall = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
     }
 
+    void Flip()
+    {
+        transform.localScale = new(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        SwapFacingRight();
+    }
+
+    private void PerformMovement()
+    {
+        // Player can not move horizontally if it's on wall and not grounded
+        if (_isOnWall && !_isGrounded && _possess)
+        {
+            // Debug.Log("Movement Locked");
+            return;
+        }
+
+        HandleFlip();
+        _rb.velocity = new Vector2(_moveInput * speed, _rb.velocity.y);
+        // if(!_verticalMovement)
+        // _rb.velocity = transform.right * speed * _moveInput;
+        // else
+        //     _rb.velocity = new Vector2(_rb.velocity.x, _moveInput * speed) ;
+    }
+
     private void HandleWallSlide()
     {
-        if (_isOnWall && !_isGrounded && _moveInput != 0)
+        // if (_isOnWall && !_isGrounded && _moveInput != 0)
+        if (Input.GetKeyDown(KeyCode.LeftControl) && _isGhost)
         {
-            _wallSliding = true;
+            _possess = true;
+            GetComponent<BoxCollider2D>().size += new Vector2(k_PossessColliderXOffset, 0);
+            frontCheck.localPosition += new Vector3(k_PossessFrontCheckerXOffset, 0, 0);
+
+            // _wallSliding = true;
+            // if(_isOnWall)
+            //     _wallGrab = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            // _wallGrab = false;
+            _possess = false;
+            GetComponent<BoxCollider2D>().size -= new Vector2(k_PossessColliderXOffset, 0);
+            frontCheck.localPosition -= new Vector3(k_PossessFrontCheckerXOffset, 0, 0);
+
+            transform.rotation = Quaternion.identity;
+            _verticalMovement = false;
+            _rb.gravityScale = k_GravityScale;
+
+            if (inverseMovement)
+            {
+                SwapFacingRight();
+                inverseMovement = false;
+            }
+        }
+
+        if (_possess && _isOnWall)
+        {
+            if (!inverseMovement)
+                transform.Rotate(Vector3.forward, _facingRight ? 90f : -90);
+            else
+                transform.Rotate(Vector3.forward, _facingRight ? -90f : 90);
+
+
+            if (inverseMovement && transform.rotation.eulerAngles.z is 0 or 90)
+            {
+                SwapFacingRight();
+                inverseMovement = false;
+            }
+            else if (!inverseMovement && transform.rotation.eulerAngles.z is 270 or 180)
+            {
+                SwapFacingRight();
+                inverseMovement = true;
+            }
+            // This is the code to fix incorrect behaviour when walking on left wall
+            // if (!_facingRight)
+            // {
+            //     SwapFacingRight()
+            // }
+
+            _isOnWall = false;
+            _verticalMovement = !_verticalMovement;
         }
         else
         {
-            _wallSliding = false;
         }
 
-        if (_wallSliding)
+        // Handle Vertical Movement
+        if (_isGrounded && _possess && _verticalMovement)
         {
-            _rb.velocity = new Vector2(_rb.velocity.x,
-                Mathf.Clamp(_rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            _rb.gravityScale = 0;
+            _rb.velocity = new Vector2(0, _verticalInput * speed);
         }
+
+        // if (_wallSliding)
+        // {
+        //     _rb.velocity = new Vector2(_rb.velocity.x,
+        //         Mathf.Clamp(_rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        // }
+    }
+
+    private void SwapFacingRight()
+    {
+        _facingRight = !_facingRight;
     }
 
     private void HandleWallJump()
@@ -166,17 +297,6 @@ public class PlayerController : MonoBehaviour
         {
             _rb.velocity = new Vector2(xWallForce * -_moveInput, yWallForce);
         }
-    }
-
-    private void HandleMovement()
-    {
-        _rb.velocity = new Vector2(_moveInput * speed, _rb.velocity.y);
-    }
-
-    void Flip()
-    {
-        transform.localScale = new(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        _facingRight = !_facingRight;
     }
 
     void ResetWallJumping()
--- GameJam GameDevTV/Assets/Scripts/SwitchGravity.cs
+++ GameJam GameDevTV/Assets/Scripts/SwitchGravity.cs
@@ -6,37 +6,59 @@ using UnityEngine;
 public class SwitchGravity : MonoBehaviour
 {
    private Rigidbody2D _rb;
+   private PlayerController _player;
 
+   private GravityState gravityCurrentState = GravityState.Down;
+
+   private readonly Vector2 k_DownGravity = new(0f, -9.81f);
+   private readonly Vector2 k_UpGravity = new(0f, 9.81f);
+   private readonly Vector2 k_LeftGravity = new(-9.81f, 0f);
+   private readonly Vector2 k_RightGravity = new(9.81f, 0f);
+   
    private void Start()
    {
       _rb = GetComponent<Rigidbody2D>();
+      _player = GetComponent<PlayerController>();
    }
 
    private void Update()
    {
-      if (Input.GetKeyDown(KeyCode.I))
+      if (gravityCurrentState != GravityState.Up && Input.GetKeyDown(KeyCode.I))
       {
-         Physics2D.gravity = new Vector2(0f, 9.81f);
+         Physics2D.gravity = k_UpGravity;
          transform.eulerAngles = new Vector3(0, 0, 180);
+         // _player.FacingRight = !_player.FacingRight;
+         gravityCurrentState = GravityState.Up;
+         
+         _player.VerticalMovement = false;
       }
-      if (Input.GetKeyDown(KeyCode.K))
+      else if (gravityCurrentState != GravityState.Down && Input.GetKeyDown(KeyCode.K))
       {
-         Physics2D.gravity = new Vector2(0f, -9.81f);
+         Physics2D.gravity = k_DownGravity;
          transform.eulerAngles = Vector3.zero;
-
+         // _player.FacingRight = !_player.FacingRight;
+         gravityCurrentState = GravityState.Down;
+         
+         _player.VerticalMovement = false;
       }
-      if (Input.GetKeyDown(KeyCode.J))
+      else if (gravityCurrentState != GravityState.Left && Input.GetKeyDown(KeyCode.J))
       {
-         Physics2D.gravity = new Vector2(-9.81f, 0f);
+         Physics2D.gravity = k_LeftGravity;
          transform.eulerAngles = new Vector3(0, 0, -90);
+         gravityCurrentState = GravityState.Left;
 
+         _player.VerticalMovement = true;
       }
-      if (Input.GetKeyDown(KeyCode.L))
+      else if (gravityCurrentState != GravityState.Right && Input.GetKeyDown(KeyCode.L))
       {
-         Physics2D.gravity = new Vector2(9.81f, 0f);
+         Physics2D.gravity = k_RightGravity;
          transform.eulerAngles = new Vector3(0, 0, 90);
-
+         gravityCurrentState = GravityState.Right;
+         
+         _player.VerticalMovement = true;
       }
       
    }
 }
+
+public enum GravityState{Up,Down,Left,Right}
